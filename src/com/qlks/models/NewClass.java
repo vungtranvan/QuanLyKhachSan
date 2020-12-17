2a34257383943be112cd1b5483ba179d1d60795d
hoangdungxz <hoangtrungdungxz@gmail.com>
2020-12-17 09:42:34 +0700

    db

---
+++ src/com/qlks/models/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.qlks.models;
+
+/**
+ *
+ * @author hoangdung
+ */
+public class NewClass {
+    
+}
