35ca7aa34574900c51cf3dae0463783c8bf29631
hoangdungxz <hoangtrungdungxz@gmail.com>
2020-12-18 09:51:36 +0700

    fix

--- src/com/qlks/controller/MenuController.java
+++ src/com/qlks/controller/MenuController.java
@@ -6,7 +6,6 @@
 package com.qlks.controller;
 
 import com.qlks.bean.DanhMucBean;
-import com.qlks.view.IndexJPanel;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.util.List;
@@ -19,28 +18,6 @@ import javax.swing.JPanel;
  */
 public class MenuController {
 
-    private JPanel root;
-    private String kindSelected = "";
-    private List<DanhMucBean> listItem = null;
 
-    public MenuController(JPanel root) {
-        this.root = root;
-    }
-    
-    
-    
-    
-        public void setView(JPanel pnlItem, JLabel lblItem) {
-        kindSelected = "TrangChu";
-        pnlItem.setBackground(new Color(96, 100, 191));
-        lblItem.setBackground(new Color(96, 100, 191));
-
-        root.removeAll();
-        root.setLayout(new BorderLayout());
-        IndexJPanel indexJPanel = new IndexJPanel();
-        root.add(indexJPanel);
-        root.validate();
-        root.repaint();
     }
 
-}
